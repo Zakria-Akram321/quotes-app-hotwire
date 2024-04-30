a283a462f78943a46ce23205269b562bfe250f6b
Zakria-Akram321 <zakria.akram999@gmail.com>
2024-04-30 20:45:57 +0500

    Real time changes with Broadcast

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,7 @@
 class Quote < ApplicationRecord
+
+  after_create_commit -> {broadcast_prepend_to "all_quotes"}
+  after_destroy_commit -> {broadcast_remove_to "all_quotes"}
+
   validates :quote, presence: true
 end
