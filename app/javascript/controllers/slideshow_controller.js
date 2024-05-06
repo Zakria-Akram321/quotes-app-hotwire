cbfba4ce6e67a3421a6cc76d56419e8686327baf
Zakria-Akram321 <zakria.akram999@gmail.com>
2024-05-06 20:26:21 +0500

    Slide show is completed

--- app/javascript/controllers/slideshow_controller.js
+++ app/javascript/controllers/slideshow_controller.js
@@ -1,27 +1,55 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["slide"];
+  static targets = ["slide", "btn", "totalSlides"];
+  static values = { index: Number };
 
-  initialize() {
-    this.index = 0;
-    this.showCurrentSlide();
+  connect() {
+    this.updateTotalSlides();
   }
 
   next() {
-    this.index++
-    this.showCurrentSlide();
-    console.log(this.index)
-  }
+    if (this.indexValue < 3) {
+      this.indexValue++;
+    };
+    this.checkButtons();
+    this.updateTotalSlides();
+  };
 
   prev() {
-    this.index--;
+    if (this.indexValue > 0) {
+      this.indexValue--;
+    };
+    this.checkButtons();
+    this.updateTotalSlides();
+  };
+
+  indexValueChanged() {
     this.showCurrentSlide();
-  }
+  };
 
   showCurrentSlide() {
-    this.slideTargets.forEach(element => {
-      element.hidden = index !== this.index;
+    this.slideTargets.forEach((element, index) => {
+      element.hidden = index !== this.indexValue;
     });
-  }
+  };
+
+  checkButtons() {
+    if (this.indexValue === 0) {
+      this.btnTargets[0].disabled = true;
+    } else {
+      this.btnTargets[0].disabled = false;
+    };
+
+    if (this.indexValue === this.slideTargets.length - 1) {
+      this.btnTargets[1].disabled = true;
+    } else {
+      this.btnTargets[1].disabled = false;
+    };
+  };
+
+  updateTotalSlides() {
+    this.totalSlidesTarget.textContent = `${this.indexValue + 1}/${this.slideTargets.length}`
+    // console.log("first")
+  };
 }
